0b19e6681affe47bac266bb234c83ba4e9b99f1f
Rafhael Ronny <rafhaelronnyst@gmail.com>
2019-02-13 21:12:38 -0300

    Implementando metodo que valida a selecao feita pelo jogador

--- Assets/Scripts/WordSelectionController.cs
+++ Assets/Scripts/WordSelectionController.cs
@@ -29,26 +29,46 @@ public class WordSelectionController : MonoBehaviour
         }
         if (Input.GetMouseButtonUp(0))
         {
-            lastPos = ToGridPos(firstSlot.transform.position);
-            Debug.Log(SelectionPath());
+            lastPos = ToGridPos(lastSlot.transform.position);
         }
+    }
+
+    private bool ValidSelection()
+    {
+        return SelectionLenght(SelectionPath()) >= 0;
     }
 
     private int SelectionPath()
     {
+        int aux = Mathf.Max(level.columns, level.lines);
         for (int i = (int)firstPos.x; i < level.columns; i++)
         {
-            if (new Vector3((int)firstPos.x + i, (int)firstPos.y + i) == lastPos)
-                return 0; // Diagonal
+            for (int k = 0; k < aux; k++)
+            {
+                if (new Vector3((int)firstPos.x + k, (int)firstPos.y + k) == lastPos)
+                    return 0; // Diagonal
+            }
 
             if (new Vector3(i, (int)firstPos.y) == lastPos)
                 return 1; // Horizontal
 
             for (int j = (int)firstPos.y; j < level.lines; j++)
+            {
                 if (new Vector3((int)firstPos.x, j) == lastPos)
                     return 2; // Vertical
+            }
         }
         return -1; // None
+    }
+
+    private int SelectionLenght(int path)
+    {
+        if (path == 0)
+            return (int)(lastPos.x - firstPos.x) + 1;
+        else if (path == 1 || path == 2)
+            return (int)((lastPos.x - firstPos.x) + (lastPos.y - firstPos.y)) + 1;
+        else
+            return -1;
     }
 
     private Vector3 ToGridPos(Vector3 position)
